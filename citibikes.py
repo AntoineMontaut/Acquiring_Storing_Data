bab9363b904ead5f47213dbfd686c7e02a8c201e
AntoineMontaut <antoine.montaut@gmail.com>
2016-10-11 00:48:18 -0500

    update

--- citibikes.py
+++ citibikes.py
@@ -9,11 +9,16 @@ import datetime
 from dateutil.parser import parse
 import collections
 import time
+import pandas as pd
 
 url = 'http://www.citibikenyc.com/stations/json'
 db_name = 'citi_bike.db'
 table_name = 'available_bikes'
 avail_bikes = collections.defaultdict(int)
+# stations = requests.get(url).json()['stationBeanList']
+# id_list = []
+# for station in stations:
+    # id_list.append(station['id'])
 
 
 def clean_table(db_name, table_name):
@@ -46,7 +51,7 @@ def select_data():
     con = lite.connect(db_name)
     cur = con.cursor()
     with con:
-        cur.execute('SELECT execution_time, _{}, _{}, _{} FROM available_bikes'.format(avail_bikes.keys()[0], avail_bikes.keys()[1], avail_bikes.keys()[2]))
+        cur.execute('SELECT execution_time, _{}, _{}, _{} FROM available_bikes'.format(id_list[0], id_list[1], id_list[2]))
         print(cur.fetchall())
     
     
@@ -63,8 +68,8 @@ def test_timer():
         i += 1
 
    
-def main():
-    '''main'''
+def main_acquire_for_1hr():
+    '''acquire available bikes at stations every minute for 1 hour'''
     #CAUTION: clean_table() will erase all rows in the db!
     # clean_table(db_name, table_name) 
     
@@ -80,8 +85,44 @@ def main():
         time.sleep(60 - processing_time)
     print('End of data acquisition')
     
-    
-    
-# main()
+# main_acquire_for_1hr()
 # select_data()
-# test_timer()
+
+
+'''DATA ANALYSIS'''
+
+def sql_to_DataFrame():
+    '''create a DataFrame with previously acquired data stored in database'''
+    con = lite.connect(db_name)
+    df = pd.read_sql_query('SELECT * FROM available_bikes ORDER BY execution_time', con, index_col='execution_time')
+    stations = (df.columns.values)
+    
+    '''get activity for each station: both bikes in and bikes out count positively towards activity'''
+    df_activity = abs(df - df.shift(1))
+    # df_activity.loc[df.index[0]] = 0
+    activity = df_activity.sum()
+    def verification():
+        print(df[[stations[0], stations[1], stations[2]]])
+        print(activity[[stations[0], stations[1], stations[2]]])
+    # verification()
+    
+    most_active_station = activity.idxmax()
+    most_active_station_activity = int(activity[most_active_station])
+    
+    initial_time = int(df.index[0]) + 5*60*60 # because epoch time is at gmt 0 and time of acquisition was at gmt -5
+    initial_time = datetime.datetime.fromtimestamp(initial_time) #+ datetime.datetime(1970, 1, 1)
+    finish_time = int(df.index[-1]) + 5*60*60
+    finish_time = datetime.datetime.fromtimestamp(finish_time)
+    print('\nOn {0}, from {1} to {2}, the most active station was station {3} with a total activity of {4}.\n\
+\nNote: activity is defined as the sum of the absolute value of the variation of the number of available bikes \
+every minute for an hour.'.format(
+    initial_time.strftime('%m-%d-%Y'), initial_time.strftime('%H:%M:%S'), finish_time.strftime('%H:%M:%S'),
+    most_active_station[1:], most_active_station_activity))
+    
+    
+    
+    
+def main_data_analysis():
+    sql_to_DataFrame()
+    
+main_data_analysis()
